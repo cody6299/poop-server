976ec1dc3ebf7f1149741d3d7e85180c1d252b42
a610138467 <610138467@qq.com>
2023-06-01 01:10:46 +0800

    fix

--- internal/service/price_info/ddl.go
+++ internal/service/price_info/ddl.go
@@ -5,8 +5,8 @@ import (
 )
 
 func SaveOrUpdate(dbTransaction *gorm.DB, priceInfo *PriceInfo) (int64, error) {
-    query := "INSERT INTO price_info(`chain`, `price_type`, `price_key`, `price_open`, `price_high`, `price_low`, `price_close`) VALUES(?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE `price_high` = VALUES(`price_high`), `price_low` = VALUES(`price_low`), `price_close` = VALUES(`price_close`)"
-    values := []interface{} {priceInfo.Chain, priceInfo.PriceType, priceInfo.PriceKey, priceInfo.PriceOpen, priceInfo.PriceHigh, priceInfo.PriceLow, priceInfo.PriceClose}
+    query := "INSERT INTO price_info(`chain`, `price_type`, `price_key`, `begin_time`, `end_time`, `price_open`, `price_high`, `price_low`, `price_close`) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE `price_high` = VALUES(`price_high`), `price_low` = VALUES(`price_low`), `price_close` = VALUES(`price_close`)"
+    values := []interface{} {priceInfo.Chain, priceInfo.PriceType, priceInfo.PriceKey, priceInfo.BeginTime, priceInfo.EndTime, priceInfo.PriceOpen, priceInfo.PriceHigh, priceInfo.PriceLow, priceInfo.PriceClose}
     result := dbTransaction.Exec(query, values...)
 
     if result.Error != nil {
